10aca4739b7dda51e52105f796442947e0f0f17e
Yeasin Arafath <yeasin.arafath@bjitgroup.com>
2021-07-17 23:32:50 +0600

    updated api url in point

--- youtube-video-player/src/apis/youtube.js
+++ youtube-video-player/src/apis/youtube.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const KEY = "AIzaSyBJOUJzCs_qPP118V7ux9OYr8EBx2YsA3A";
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
+    baseURL: 'your_youtube_api_goes_here',
     params: {
       part: 'snippet', 
       type: 'video',
